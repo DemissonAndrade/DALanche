c6223e96be89df1b053cc7d58ff419767a5883c7
DemissonAndrade <demissondsa@gmail.com>
2022-08-17 20:24:14 -0300

    Projeto Created

---
+++ backend/src/main/java/com/Lanchonete/demo/AppLanchoneteApplication.java
@@ -0,0 +1,13 @@
+package com.Lanchonete.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppLanchoneteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppLanchoneteApplication.class, args);
+	}
+
+}
